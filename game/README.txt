0185e4466c8b19c0327185aa54d19fe3edc6a3df
xuesong123 <xuesong.net@gmail.com>
2013-09-13 10:41:01 +0800

    modify
    modify

--- game/README.txt
+++ game/README.txt
@@ -1,11 +1,13 @@
+Welcome !
+Usage:
 1. run index.bat
 2. open: http://localhost/index
 
-start: F2
-pause: Enter
+      start: F2
+      pause: Enter
 
-left: A or LEFT
-right: D or RIGHT
-down: S or DOWN
-up: W or UP
+       left: A or LEFT
+      right: D or RIGHT
+       down: S or DOWN
+         up: W or UP
 down bottom: Space
--- game/index.bat
+++ game/index.bat
@@ -1,2 +1,18 @@
 @ECHO OFF
+
+@ECHO Welcome !
+@ECHO Usage:
+@ECHO 1. run index.bat
+@ECHO 2. open: http://localhost/index
+@ECHO.
+@ECHO      start: F2
+@ECHO      pause: Enter
+
+@ECHO.
+@ECHO       left: A or LEFT
+@ECHO      right: D or RIGHT
+@ECHO       down: S or DOWN
+@ECHO         up: W or UP
+@ECHO down bottom: Space
+
 cmd /k node "index.js"
